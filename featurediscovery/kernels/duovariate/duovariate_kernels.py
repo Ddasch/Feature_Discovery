03bf519194a99f16189b2086026f7803d6091088
Frederik Schadd <fsc@mediaan.com>
2021-12-07 14:56:17 +0100

    magnitude kernel

--- featurediscovery/kernels/duovariate/duovariate_kernels.py
+++ featurediscovery/kernels/duovariate/duovariate_kernels.py
@@ -20,3 +20,17 @@ class Difference_Kernel(Abstract_Duovariate_Kernel):
     def _transform(self, x: Union[np.ndarray, cp.ndarray]):
         x_ret = self.kernel_func(x[:,0], x[:, 1])
         return x_ret.reshape(-1,1)
+
+
+class Magnitude_Kernel(Abstract_Duovariate_Kernel):
+    def _fit(self, x: Union[np.ndarray, cp.ndarray]):
+        self.kernel_func = cp.ElementwiseKernel(
+            'float64 x, float64 y',
+            'float64 z',
+            'z = sqrt((x * x) + (y * y))',
+            'magnitude'
+        )
+
+    def _transform(self, x: Union[np.ndarray, cp.ndarray]):
+        x_ret = self.kernel_func(x[:,0], x[:, 1])
+        return x_ret.reshape(-1,1)
--- tests/test_duovariate_kernel.py
+++ tests/test_duovariate_kernel.py
@@ -2,7 +2,7 @@ import cupy as cp
 import numpy as np
 import pandas as pd
 
-from featurediscovery.kernels.duovariate.duovariate_kernels import Difference_Kernel
+from featurediscovery.kernels.duovariate.duovariate_kernels import *
 
 
 def test_diff_kernel():
@@ -28,6 +28,35 @@ def test_diff_kernel():
         [2.0],
         [3.0],
         [5.0],
+    ])
+
+    np.testing.assert_array_equal(x_quad.get(), x_exp.get())
+
+
+
+def test_magnitude_kernel():
+    x = cp.array([
+        [1.0, 1.0],
+        [2.0, 2.0],
+        [2.0, 1.0],
+        [3.0, 1.0],
+        [4.0, 1.0],
+        [4.0, -1.0],
+    ])
+
+    kernel = Magnitude_Kernel()
+
+    kernel.fit(x)
+
+    x_quad = kernel.transform(x)
+
+    x_exp = cp.array([
+        [np.sqrt(2.0)],
+        [np.sqrt(8.0)],
+        [np.sqrt(5.0)],
+        [np.sqrt(10.0)],
+        [np.sqrt(17.0)],
+        [np.sqrt(17.0)],
     ])
 
     np.testing.assert_array_equal(x_quad.get(), x_exp.get())

177cb4842eea50bd63668c633814b5946c00b9da
Frederik Schadd <fsc@mediaan.com>
2021-12-13 10:55:51 +0100

    code cleanup and documentation

--- featurediscovery/fitter/cupy_nn/layers.py
+++ featurediscovery/fitter/cupy_nn/layers.py
@@ -50,24 +50,21 @@ class Layer():
 
 
     def linear_activation_forward(self,A_prev):
-
         '''
         Arguments:
         :param A_prev:A_prev -- activations from previous layer (or input data): (size of previous layer, number of examples)
 
         :return:
-        A -- the output of the activation function, also called the post-activation value
-        cache -- a python dictionary containing "linear_cache" and "activation_cache";
-                 stored for computing the backward pass efficiently
+        A -- the output of the activation function, aka sigmoid(WX+b)
         '''
 
         assert A_prev.shape[0] == self.input_size
 
+        Z = self.linear_forward(A_prev)
+
         if self.activation_func == "sigmoid":
-            Z= self.linear_forward(A_prev)
             A = self.activation_kernel._transform(Z)
 
-            #activation_cache = (Z)
 
         assert (A.shape == (self.W.shape[0], A_prev.shape[1]))
 
@@ -75,25 +72,22 @@ class Layer():
         self.Z_cache = Z
         self.A_cache = A
 
-        #self.full_cache = (linear_cache, activation_cache)
-
         return A
 
     def _linear_backward(self, dZ):
         """
-        Implement the linear portion of backward propagation for a single layer (layer l)
+        Given derivative of cost w.r.t. the linear output activation, compute the derivative of the cost w.r.t
+        the input activation of this layer. Also computes and caches derivatives of cost w.r.t the weights and biases
+        of this layer for the optimizer to user later
 
         Arguments:
-        dZ -- Gradient of the cost with respect to the linear output (of current layer l)
-        cache -- tuple of values (A_prev, W, b) coming from the forward propagation in the current layer
+        dZ -- Gradient of the cost with respect to the linear output
 
         Returns:
         dA_prev -- Gradient of the cost with respect to the activation (of the previous layer l-1), same shape as A_prev
-        dW -- Gradient of the cost with respect to W (current layer l), same shape as W
-        db -- Gradient of the cost with respect to b (current layer l), same shape as b
         """
-        #A_prev, W, b = self.cache
 
+        #the amount of samples in the current batch
         m = self.A_prev_cache.shape[1]
 
         #these are for the gradient descent optimizer for this layer
@@ -103,10 +97,8 @@ class Layer():
         #this one needs to be passed along for the next layer
         dA_prev = cp.dot(self.W.T, dZ)
 
-
         assert (dA_prev.shape == self.A_prev_cache.shape)
         assert (dW.shape == self.W.shape)
-        #assert (isinstance(db, np.float64))
 
         self.dW = dW
         self.db = db
@@ -116,24 +108,19 @@ class Layer():
 
     def linear_activation_backward(self, dA):
         """
-        Implement the backward propagation for the LINEAR->ACTIVATION layer.
+        Given the derivative of the cost w.r.t. the output activations of this layer (including the activation function),
+        compute derivative of the cost w.r.t the inputs to this layer. Also computes and caches
+        derivatives of cost w.r.t the weights and biases of this layer for the optimizer to user later
 
         Arguments:
         dA -- post-activation gradient for current layer l
-        cache -- tuple of values (linear_cache, activation_cache) we store for computing backward propagation efficiently
-        activation -- the activation to be used in this layer, stored as a text string: "sigmoid" or "relu"
-
         Returns:
         dA_prev -- Gradient of the cost with respect to the activation (of the previous layer l-1), same shape as A_prev
-        dW -- Gradient of the cost with respect to W (current layer l), same shape as W
-        db -- Gradient of the cost with respect to b (current layer l), same shape as b
         """
-        #linear_cache, activation_cache = self.full_cache
 
         if self.activation_func == "sigmoid":
             dZ = self.sigmoid_backward(dA, self.Z_cache)
 
-        # Shorten the code
         dA_prev= self._linear_backward(dZ)
 
         return dA_prev
@@ -145,16 +132,14 @@ class Layer():
         self.b = self.b - self.db * self.learning_rate
 
 
-    def sigmoid_backward(self, dA, cache):
+    def sigmoid_backward(self, dA, Z):
         """
         The backward propagation for a single SIGMOID unit.
         Arguments:
         dA - post-activation gradient, of any shape
-        cache - 'Z' where we store for computing backward propagation efficiently
-        Returns:
+        Z - the cached Z values that were the input to this sigmoid step during the forward pass
         dZ - Gradient of the cost with respect to Z
         """
-        Z = cache
         s = 1/(1+cp.exp(-Z))
         dZ = dA * s * (1-s)
         return dZ

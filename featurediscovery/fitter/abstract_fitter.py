removed empty file demo script with demo dataset bugfix nan gradients-> implemented gradient clipping

featurediscovery/fitter/abstract_fitter.py
@@ -41,9 +41,10 @@ class Abstract_Fitter(ABC):
 
         if use_cupy:
             if len(set(cp.unique(y_hat).get()).difference(set(cp.unique(y).get()))) > 0:
+                y_hat = self._score(x)
                 raise Exception(
                     'Fitter is returning labels not in the original set. Original set is {} but fitter is returning {}'.format(
-                        set(cp.unique(y)), set(cp.unique(y_hat))))
+                        set(cp.unique(y).get()), set(cp.unique(y_hat).get())))
         else:
             if len(set(np.unique(y_hat)).difference(set(np.unique(y)))) > 0:
                 raise Exception(

featurediscovery/fitter/cupy_nn/layers.py
@@ -91,7 +91,7 @@ class Layer():
 
         return A
 
-    def _linear_backward(self, dZ):
+    def _linear_backward(self, dZ, dA_debug=None):
         '''
         Given derivative of loss w.r.t. the linear output activation, compute the derivative of the loss w.r.t
         the input activation of this layer. Also computes and caches derivatives of loss w.r.t the weights and biases
@@ -113,6 +113,39 @@ class Layer():
 
         assert (dA_prev.shape == self.A_prev_cache.shape)
         assert (dW.shape == self.W.shape)
+
+        '''
+        #NOTE: debug code to investigate nan values due to exploding gradients
+        if cp.isnan(cp.sum(dW)):
+            import numpy as np
+            dZ = self.activation_func_obj.activation_backward(dA_debug, self.Z_cache)
+
+            dZ = dZ.get()
+            A_prev_cache = self.A_prev_cache.get()
+            A_prev_cache = A_prev_cache.T
+            tmp = np.dot(dZ, A_prev_cache)
+            tmp = tmp / m
+
+
+            #manual dot product for debug
+            dZ_nan_indexi = np.argwhere(np.isnan(dZ))
+
+
+            debug_dot_product = np.zeros((dZ.shape[0], A_prev_cache.shape[1]))
+            for index_dZ in range(dZ.shape[0]):
+                for index_A_prev_cache in range(A_prev_cache.shape[1]):
+                    sum = 0
+                    for sample_index in range(dZ.shape[1]):
+                        val_dZ = dZ[index_dZ][sample_index]
+                        val_A_prev_debug = A_prev_cache[sample_index][index_A_prev_cache]
+                        if np.isnan(sum + val_dZ*val_A_prev_debug):
+                            print('')
+                        sum = sum + val_dZ*val_A_prev_debug
+
+                    debug_dot_product[index_dZ][index_A_prev_cache] = sum
+
+            print('')
+        '''
 
         self.dW = dW
         self.db = db

featurediscovery/fitter/sklearn_fitters.py
@@ -37,7 +37,7 @@ class Logistic_Scikit(Abstract_Fitter):
         if type(x) != np.ndarray or type(y) != np.ndarray:
             raise Exception('Scikit fitters require np.ndarray as input. Received {} instead'.format(type(x)))
 
-        logistic = LogisticRegression()
+        logistic = LogisticRegression(solver='liblinear')
 
         logistic.fit(x,y.reshape(-1))
 

featurediscovery/kernels/duovariate/duovariate_kernels.py
@@ -9,7 +9,7 @@ from featurediscovery.kernels.duovariate.abstract_duovariate import Abstract_Duo
 from sklearn.preprocessing import PolynomialFeatures
 
 
-SUPPORTED_DUOVARIATE_KERNELS = ['difference', 'magnitude', 'poly3', 'poly2']
+SUPPORTED_DUOVARIATE_KERNELS = ['difference', 'magnitude', 'poly3', 'poly2', 'rff', 'rff_gauss']
 
 
 def get_duo_kernel(kernel_name:str, standardizer:str) -> Abstract_Duovariate_Kernel:
@@ -27,6 +27,12 @@ def get_duo_kernel(kernel_name:str, standardizer:str) -> Abstract_Duovariate_Ker
 
     if kernel_name == 'magnitude':
         return Magnitude_Kernel(standardizer)
+
+    if kernel_name == 'rff':
+        return RFF_Kernel(standardizer)
+
+    if kernel_name == 'rff_gauss':
+        return RFF_Kernel_Gauss_Approx(standardizer)
 
 
 difference_kernel_singleton = None
@@ -208,7 +214,7 @@ class Polynomial_Second_Order_Kernel(Abstract_Duovariate_Kernel):
 
     def _get_kernel_feature_names(self, f1:str, f2:str):
         return ['{}^2'.format(f1)
-            , '{}*{}'.format(f1, f2)
+            , '2*{}*{}'.format(f1, f2)
             , '{}^2'.format(f2)]
 
     def get_kernel_name(self) -> str:
@@ -242,12 +248,26 @@ class Sigmoid_Kernel_Backwards(Abstract_Duovariate_Kernel):
         if self.api == 'cupy':
             x_ret = self.kernel_func(x[:, 0], x[:, 1])
             return x_ret
+
         if self.api == 'numpy':
             raise Exception('not yet implemented')
 
     def sigmoid_backward(self, dA: Union[np.ndarray, cp.ndarray], Z:Union[np.ndarray, cp.ndarray]):
         x_ret = self.kernel_func(dA, Z)
+
+        '''
+        #NOTE: debug code to investigate nan values due to exploding gradients
+        if cp.isnan(cp.sum(x_ret)):
+            dA = dA.get()
+            Z = Z.get()
+
+            dA_nan_indezi = np.argwhere(np.isnan(dA))
+            Z_nan_indezi = np.argwhere(np.isnan(Z))
+            print('')
+        '''
+
         return x_ret
+
 
 
     def get_kernel_name(self):
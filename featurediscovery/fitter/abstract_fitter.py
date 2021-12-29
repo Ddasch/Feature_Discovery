bugfix cupy_nn not clipping activation during backprop min_epoch and debug parameter debug code in nn model

featurediscovery/fitter/abstract_fitter.py
@@ -24,7 +24,7 @@ class Abstract_Fitter(ABC):
 
 
 
-    def compute_fit_quality(self, x: Union[np.ndarray, cp.ndarray], y:Union[np.ndarray, cp.ndarray]) -> float:
+    def compute_fit_quality(self, x: Union[np.ndarray, cp.ndarray], y:Union[np.ndarray, cp.ndarray], debug:bool=False) -> float:
 
         if len(x.shape) != 2:
             raise Exception('x must be 2 dimensional, first dimension indicating the sample index and second the feature index')
@@ -38,7 +38,7 @@ class Abstract_Fitter(ABC):
 
         use_cupy = type(x) == cp.ndarray
 
-        self._fit(x,y)
+        self._fit(x,y, debug=debug)
 
         y_hat = self._score(x)
 
@@ -121,7 +121,7 @@ class Abstract_Fitter(ABC):
         return x_decision_boundary, y_hat_decision_boundary
 
     @abstractmethod
-    def _fit(self, x: Union[np.ndarray, cp.ndarray], y:Union[np.ndarray, cp.ndarray]):
+    def _fit(self, x: Union[np.ndarray, cp.ndarray], y:Union[np.ndarray, cp.ndarray], debug:bool=False):
         pass
 
     @abstractmethod

featurediscovery/fitter/cupy_fitter.py
@@ -16,11 +16,16 @@ class Logistic_Regression_ANN(Abstract_Fitter):
                          , output_activation='sigmoid'
                          , hidden_activations=None
                          , hidden_layer_sizes=None
-                         , learning_rate=0.04
+                         , learning_rate=0.1
                          , better_weight_init_method='magnified_corr')
 
-    def _fit(self, x: Union[np.ndarray, cp.ndarray], y:Union[np.ndarray, cp.ndarray]):
-        self.model.fit(x,y,n_epoch=300, cost_improvement_thresh=0.0001, cost_improvement_agg_range=5, verbose=False)
+    def _fit(self, x: Union[np.ndarray, cp.ndarray], y:Union[np.ndarray, cp.ndarray], debug:bool=False):
+        self.model.fit(x, y
+                       , max_epoch=500
+                       , min_epoch=5
+                       , cost_improvement_thresh=0.00001
+                       , cost_improvement_agg_range=10
+                       , verbose=False, debug=debug)
 
 
     def _score(self, x: Union[np.ndarray, cp.ndarray]):

featurediscovery/fitter/cupy_nn/costs.py
@@ -36,12 +36,25 @@ class CrossEntropyCost():
 
         return cost
 
-    def loss_backward(self, AL, Y):
+    def loss_backward(self, AL, Y, debug:bool=False):
         #source: http://www.adeveloperdiary.com/data-science/deep-learning/neural-network-with-softmax-in-python/
         #return cp.subtract(AL,Y)
         #NOTE: BAD SOURCE!, incorrect!
+
+        AL = cp.clip(AL, self.epsilon, 1 - self.epsilon)
         unclipped = - (cp.divide(Y, AL) - cp.divide(1 - Y, 1 - AL))
         if self.gradient_clipping_val is not None:
             unclipped[unclipped > self.gradient_clipping_val] = self.gradient_clipping_val
             unclipped[unclipped < - self.gradient_clipping_val] = -self.gradient_clipping_val
+
+        if debug:
+            import numpy as np
+            AL = AL.get()
+            Y = Y.get()
+            first_part_loss = np.divide(Y, AL)
+            second_part_loss = - np.divide(1 - Y, 1 - AL)
+            loss = -(first_part_loss - second_part_loss)
+
+
+
         return unclipped

featurediscovery/fitter/cupy_nn/models.py
@@ -77,7 +77,7 @@ class ANN():
                  hidden_activations: List[str] = None,
                  better_weight_init_method:str = None,
                  optimizer:str='adam',
-                 gradient_clipping_val:float=5.0
+                 gradient_clipping_val:float=1.0
                  ):
 
         if hidden_layer_sizes is None:
@@ -191,13 +191,17 @@ class ANN():
 
         self.layers.append(output_layer)
 
-    def fit(self, x: cp.ndarray, y: cp.ndarray, n_epoch:int=20
+    def fit(self, x: cp.ndarray, y: cp.ndarray
+            , max_epoch:int=100
+            , min_epoch:int=20
             , cost_improvement_thresh:float=0.001
             , cost_improvement_agg_range:int=5
-            , verbose:bool=False):
+            , verbose:bool=False
+            , debug:bool=False
+            ):
 
-        if n_epoch is None:
-            n_epoch = 99999999
+        if max_epoch is None:
+            max_epoch = 100
 
         # shape x: [n_sample, n_feature], standard format what you get out of a df
         X = x.transpose()
@@ -210,7 +214,7 @@ class ANN():
 
         cost_history = cp.ones((cost_improvement_agg_range))*99999
 
-        for epoch in range(n_epoch):
+        for epoch in range(max_epoch):
 
             #forward pass
             A = X
@@ -225,7 +229,7 @@ class ANN():
                 print('Cost at epoch {} : {}'.format(epoch, current_cost))
 
             if cost_improvement_thresh is not None:
-                if cost_history_agg - current_cost <= cost_improvement_thresh:
+                if cost_history_agg - current_cost <= cost_improvement_thresh and epoch > min_epoch:
                     break
 
             cost_history[:-1] = cost_history[1:]
@@ -233,6 +237,13 @@ class ANN():
 
             #compute derivative of loss with respect to last activation
             dL_A = self.cost_function.loss_backward(A, Y)
+
+            if debug:
+                import numpy as np
+                A_tmp = A.get()
+                dL_A_tmp = dL_A.get()
+                dL_A_nan_indexi =  np.argwhere(np.isnan(dL_A_tmp))
+                dL_A = self.cost_function.loss_backward(A, Y, debug=True)
 
             #abort backprop if derivatives have a nan
             if cp.isnan(cp.sum(dL_A)):

scripts/demo.py
@@ -6,14 +6,19 @@ from featurediscovery.kernel_search import evaluate_kernels
 
 def generate_demo_dataset(no_samples:int):
 
+    np.random.seed(1)
 
     # Engineer a 2nd order polynomial solution
     x1 = np.random.uniform(-10, 10, no_samples)
     x2 = np.random.uniform(-10, 10, no_samples)
 
-    poly2 = 0.1*x1*x1 - 10*x1*x2 + 2*x2*x2
-    y_poly2 = np.zeros(no_samples)
-    y_poly2[poly2 > 100] = 0.0
+    #poly2 = 0.1*x1*x1 - 10*x1*x2 + 2*x2*x2
+    #y_poly2 = np.zeros(no_samples)
+    #y_poly2[poly2 > 100] = 0.6
+
+    poly3 = 1 * x1 * x1 * x1 - 3 * x1 * x1 * x2 + 3 * x1 * x2 * x2 - 1 * x2 * x2 * x2
+    y_poly3 = np.zeros(no_samples)
+    y_poly3[poly3 > 1.5] = 0.6
 
     #add some noise to pattern
     #x1 = x1 + np.random.randn(no_samples)
@@ -70,7 +75,7 @@ def generate_demo_dataset(no_samples:int):
         data['x{}'.format(feature_index)] = x_noise3[:,i]
         feature_index = feature_index + 1
 
-    y = y_poly2 + y_cosine
+    y = y_poly3 + y_cosine
     y[y > 0.5] = 1
     y[y < 0.5] = 0
 
@@ -82,7 +87,7 @@ def generate_demo_dataset(no_samples:int):
 
 if __name__ == '__main__':
 
-    df = generate_demo_dataset(1000)
+    df = generate_demo_dataset(10000)
 
     #feature_space = ['x0','x1','x2','x3','x4','x5','x6','x7','x8','x9','x10','x11','x12']
     feature_space = ['x0', 'x1', 'x2', 'x3', 'x4', 'x5']
@@ -90,11 +95,11 @@ if __name__ == '__main__':
                      , target_variable='y'
                      , feature_space=feature_space
                      , monovariate_kernels=None
-                     , duovariate_kernels=['poly2', 'poly3', 'rff_gauss']
-                     , feature_standardizers=['minmax', 'raw', 'standard']
+                     , duovariate_kernels=['poly3']
+                     , feature_standardizers=['raw']
                      , plot_ranking=True
                      , eval_method='full'
-                     , use_cupy='no'
+                     , use_cupy='yes'
                      )
 
 
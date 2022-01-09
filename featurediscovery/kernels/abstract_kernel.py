d42f38db45fd6838488adaca02e3b12c157a7d6b
Frederik Schadd <fsc@mediaan.com>
2022-01-09 17:29:28 +0100

    copyright notices

--- featurediscovery/kernels/abstract_kernel.py
+++ featurediscovery/kernels/abstract_kernel.py
@@ -6,6 +6,17 @@ import numpy as np
 import pandas as pd
 
 from featurediscovery.standardizers.standardizers import get_scaler
+# Copyright 2021-2022 by Frederik Christiaan Schadd
+# All rights reserved
+#
+# Licensed under the GNU Lesser General Public License version 2.1 ;
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://opensource.org/licenses/LGPL-2.1
+#
+# or consult the LICENSE file included in the project.
+
 from featurediscovery.standardizers.abstract_standardizer import Abstract_Standardizer
 
 
--- featurediscovery/kernels/monovariate/monovariate_kernels.py
+++ featurediscovery/kernels/monovariate/monovariate_kernels.py
@@ -1,3 +1,14 @@
+# Copyright 2021-2022 by Frederik Christiaan Schadd
+# All rights reserved
+#
+# Licensed under the GNU Lesser General Public License version 2.1 ;
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://opensource.org/licenses/LGPL-2.1
+#
+# or consult the LICENSE file included in the project.
+
 
 
 from typing import Union
